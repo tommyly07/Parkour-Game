fb7181938bddde1cd05cbc25439864e323ccf1fa
tommyly07 <krylinnakhmer@gmail.com>
2026-03-29 22:38:09 +0200

    Kamera verfolgt Spieler, also entweder Raum Kamera oder Player Kamera kann man aussuchen im code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //Raum Kamera
     [SerializeField]private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Player Kamera
+    [SerializeField]private Transform player;
+    [SerializeField]private float aheadDistance;
+    [SerializeField]private float cameraspeed;
+    private float lookAhead;
+
 
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, 
-        new Vector3(currentPosX, transform.position.y, transform.position.z), 
-        ref velocity, 
-        speed);
+        //Raum Kamera
+        //transform.position = Vector3.SmoothDamp(transform.position, 
+        //new Vector3(currentPosX, transform.position.y, transform.position.z), 
+        //ref velocity, 
+        //speed);
+
+        //Player verfolgen
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraspeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
